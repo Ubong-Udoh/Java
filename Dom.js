b117e4e5c5b166c35adb801fba19a7777bcd1a2c
Ubong-Udoh <uudoh358@gmail.com>
2025-02-13 09:09:27 +0100

    Add files via upload

---
+++ Dom.js
@@ -0,0 +1,22 @@
+let box = document.getElementById("box");
+box.style.background = "blue";
+let button = document.getElementById("");
+box.style.border = "2px solid black";
+box.style.width = "200px";  
+box.style.height = "200px"; 
+document.body.style.backgroundColor = "";
+document.body.style.color = "";
+button.addEventListener("click", function() {});
+
+
+function myfunction(){
+let x = document.getElementsByName("name").values;
+let text;
+if (isNaN(x)|| x < 1|| x > 20) {
+    text = "input not valid";
+    alert ="fill in the space";     
+} else {
+    text = "input correct";
+}
+document.getElementsByName("demo");
+}
